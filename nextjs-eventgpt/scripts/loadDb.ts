a19e4f166c0d1acc529df465cf3f30fac29daaea
Soham-gavas <sohamgavas28@gmail.com>
2025-03-26 20:09:35 +0530

    Update loadDb.ts

--- nextjs-eventgpt/scripts/loadDb.ts
+++ nextjs-eventgpt/scripts/loadDb.ts
@@ -8,7 +8,15 @@ import "dotenv/config"
 
 type SimilarityMetric = "dot_product" | "cosine" | "euclidean"
 
+const ASTRA_DB_NAMESPACE="default_keyspace"
+const ASTRA_DB_COLLECTION="eventmastergpt"
+const ASTRA_DB_API_ENDPOINT="https://7bb02b78-a11a-45cb-ba05-96f4da9092d1-us-east-2.apps.astra.datastax.com"
+const ASTRA_DB_APPLICATION_TOKEN="AstraCS:mdqPwHvFfLioneglrUZawQTh:98e0c045ab63ad961dbf850fdbfdc6817a60a57bad19663353808b727dd15200"
+const OPENAI_API_KEY_2="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"
 
+const OPENAI_API_KEY="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"
+const OPENAI_API_ENDPOINT="https://models.inference.ai.azure.com"
+const ASTRA_DB_URL="https://default_keyspace.7bb02b78-a11a-45cb-ba05-96f4da9092d1-us-east-2.apps.astra.datastax.com"
 
 
 
